dfd7603c9237240eb40d25f522419ba3861139e0
John Kapantzakis <kapantzak@gmail.com>
2024-09-09 10:43:24 +0300

    [DEBUG]

--- src/shared/hooks/useGetChartData.ts
+++ src/shared/hooks/useGetChartData.ts
@@ -32,48 +32,68 @@ export const useGetChartData = async ({
   from,
   to,
 }: UseGetChartDataType) => {
-  let aggregations = [];
+  // let aggregations = [];
 
-  switch (groupBy) {
-    case 'node':
-      aggregations = [
-        { method: 'sum', groupBy: ['chart', 'node'] },
-        { method, groupBy: ['node'] },
-      ];
-      break;
-    case 'dimension':
-      aggregations = [{ method, groupBy: ['dimension'] }];
-      break;
-    case 'instance':
-      aggregations = [{ method: 'sum', groupBy: ['chart', 'node'] }];
-      break;
-    default:
-      aggregations = [
-        { method: 'sum', groupBy: ['chart', `label=${groupBy}`] },
-        { method: 'avg', groupBy: [`label=${groupBy}`] },
-      ];
-      break;
-  }
+  // switch (groupBy) {
+  //   case 'node':
+  //     aggregations = [
+  //       { method: 'sum', groupBy: ['chart', 'node'] },
+  //       { method, groupBy: ['node'] },
+  //     ];
+  //     break;
+  //   case 'dimension':
+  //     aggregations = [{ method, groupBy: ['dimension'] }];
+  //     break;
+  //   case 'instance':
+  //     aggregations = [{ method: 'sum', groupBy: ['chart', 'node'] }];
+  //     break;
+  //   default:
+  //     aggregations = [
+  //       { method: 'sum', groupBy: ['chart', `label=${groupBy}`] },
+  //       { method: 'avg', groupBy: [`label=${groupBy}`] },
+  //     ];
+  //     break;
+  // }
 
   return await Post({
     path: `/v3/spaces/${spaceId}/rooms/${roomId}/data`,
     baseUrl,
     data: {
-      filter: {
-        nodeIDs: nodes,
-        context: contextId,
-        dimensions,
-        ...(filterBy && filterValue ? { labels: { [filterBy]: [filterValue] } } : {}),
+      format: 'json2',
+      options: ['jsonwrap', 'nonzero', 'flip', 'ms', 'jw-anomaly-rates', 'minify'],
+      scope: {
+        contexts: ['mem.thp_details'],
+        nodes: [
+          '01702efd-af72-45b0-8e2f-33d806e5699f',
+          '0a841ab0-bc12-41a0-894a-e41fd5440eb8',
+          'ce0e8300-8270-4171-ae48-c452d3b91ba9',
+          'e0d74612-cf5a-4030-959c-de5dee3d793a',
+          '3ed8bb08-f2d3-4054-b423-67a8313fb130',
+          'af4b47bb-d6be-4a46-b6b0-0a735dbc0a4c',
+          '2626d41a-39f1-407f-8965-c08d146e8c4d',
+          'e48415db-3d25-4521-982c-30e9f46c96d2',
+          '382c7e2a-4b64-46ba-b8a5-f5f75eafa3e9',
+          '8b9cc340-6d60-47d9-99d5-de43071de8ba',
+        ],
       },
-      aggregations,
-      agent_options: ['jsonwrap', 'flip', 'ms'],
-      points: 335,
-      format: 'json',
-      group,
-      gtime: 0,
-      after: from,
-      before: to,
-      with_metadata: true,
+      selectors: {
+        contexts: ['*'],
+        nodes: ['*'],
+        instances: ['*'],
+        dimensions: ['*'],
+        labels: ['*'],
+      },
+      aggregations: {
+        metrics: [
+          {
+            group_by: ['dimension'],
+            group_by_label: [],
+            aggregation: 'sum',
+          },
+        ],
+        time: { time_group: 'average', time_resampling: 0 },
+      },
+      window: { after: 1722412818, before: 1722413718, points: 269 },
     },
   });
 };
