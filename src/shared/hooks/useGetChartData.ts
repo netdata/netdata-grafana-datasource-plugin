fa3df389a751798b64d14f036e106a17d1abf409
John Kapantzakis <kapantzak@gmail.com>
2024-09-09 11:40:05 +0300

    [wip] Refactor data payload

--- src/shared/hooks/useGetChartData.ts
+++ src/shared/hooks/useGetChartData.ts
@@ -31,27 +31,82 @@ export const useGetChartData = async ({
   dimensions = [],
   from,
   to,
+  ...rest
 }: UseGetChartDataType) => {
-  let aggregations = [];
+  console.log({
+    baseUrl,
+    roomId,
+    nodes,
+    spaceId,
+    contextId,
+    filterBy,
+    filterValue,
+    groupBy,
+    method,
+    group,
+    dimensions,
+    from,
+    to,
+    ...rest,
+  });
+  let metrics = [];
 
   switch (groupBy) {
     case 'node':
-      aggregations = [
-        { method: 'sum', groupBy: ['chart', 'node'] },
-        { method, groupBy: ['node'] },
+      metrics = [
+        {
+          group_by: ['chart', 'node'],
+          group_by_label: [],
+          aggregation: 'sum',
+        },
+        {
+          group_by: ['node'],
+          group_by_label: [],
+          aggregation: method,
+        },
       ];
+      // metrics = [
+      //   { method: 'sum', groupBy: ['chart', 'node'] },
+      //   { method, groupBy: ['node'] },
+      // ];
       break;
     case 'dimension':
-      aggregations = [{ method, groupBy: ['dimension'] }];
+      metrics = [
+        {
+          group_by: ['dimension'],
+          group_by_label: [],
+          aggregation: method,
+        },
+      ];
+      // metrics = [{ method, groupBy: ['dimension'] }];
       break;
     case 'instance':
-      aggregations = [{ method: 'sum', groupBy: ['chart', 'node'] }];
+      metrics = [
+        {
+          group_by: ['chart', 'node'],
+          group_by_label: [],
+          aggregation: 'sum',
+        },
+      ];
+      // metrics = [{ method: 'sum', groupBy: ['chart', 'node'] }];
       break;
     default:
-      aggregations = [
-        { method: 'sum', groupBy: ['chart', `label=${groupBy}`] },
-        { method: 'avg', groupBy: [`label=${groupBy}`] },
+      metrics = [
+        {
+          group_by: ['chart'],
+          group_by_label: groupBy,
+          aggregation: 'sum',
+        },
+        {
+          group_by: [],
+          group_by_label: groupBy,
+          aggregation: 'avg',
+        },
       ];
+      // metrics = [
+      //   { method: 'sum', groupBy: ['chart', `label=${groupBy}`] },
+      //   { method: 'avg', groupBy: [`label=${groupBy}`] },
+      // ];
       break;
   }
 
@@ -59,21 +114,46 @@ export const useGetChartData = async ({
     path: `/v3/spaces/${spaceId}/rooms/${roomId}/data`,
     baseUrl,
     data: {
-      filter: {
-        nodeIDs: nodes,
-        context: contextId,
-        dimensions,
-        ...(filterBy && filterValue ? { labels: { [filterBy]: [filterValue] } } : {}),
+      format: 'json2',
+      options: ['jsonwrap', 'flip', 'ms'],
+      scope: {
+        contexts: [contextId],
+        nodes,
       },
-      aggregations,
-      agent_options: ['jsonwrap', 'flip', 'ms'],
-      points: 335,
-      format: 'json',
-      group,
-      gtime: 0,
-      after: from,
-      before: to,
-      with_metadata: true,
+      selectors: {
+        contexts: ['*'],
+        nodes: ['*'],
+        instances: ['*'],
+        dimensions: ['*'],
+        labels: ['*'],
+      },
+      aggregations: {
+        metrics,
+        time: { time_group: group, time_resampling: 0 },
+      },
+      window: { after: from, before: to, points: 269 },
     },
   });
+
+  // return await Post({
+  //   path: `/v3/spaces/${spaceId}/rooms/${roomId}/data`,
+  //   baseUrl,
+  //   data: {
+  //     filter: {
+  //       nodeIDs: nodes,
+  //       context: contextId,
+  //       dimensions,
+  //       ...(filterBy && filterValue ? { labels: { [filterBy]: [filterValue] } } : {}),
+  //     },
+  //     aggregations,
+  //     agent_options: ['jsonwrap', 'flip', 'ms'],
+  //     points: 335,
+  //     format: 'json',
+  //     group,
+  //     gtime: 0,
+  //     after: from,
+  //     before: to,
+  //     with_metadata: true,
+  //   },
+  // });
 };
