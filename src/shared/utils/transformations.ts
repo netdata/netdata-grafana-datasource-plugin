cad2253e709d01f477b3d4ef4784d6dc946e72e8
John Kapantzakis <kapantzak@gmail.com>
2024-09-12 15:31:57 +0300

    Fix linting error

--- src/shared/utils/transformations.ts
+++ src/shared/utils/transformations.ts
@@ -2,14 +2,20 @@ import { GroupByList } from 'shared/constants';
 
 export const getDimensions = (dimensions: any) => {
   const { ids, names } = dimensions;
-  if (!ids) return [];
+  if (!ids) {
+    return [];
+  }
+
   return ids.map((id: string, index: number) => ({ value: id, label: names[index] || id }));
 };
 
 export const defaultFilter = { 'No filter': [] };
 
 export const getFilters = (labels: any[]) => {
-  if (!labels?.length) return defaultFilter;
+  if (!labels?.length) {
+    return defaultFilter;
+  }
+
   return {
     ...defaultFilter,
     ...labels.reduce((acc: any, label: any) => {
